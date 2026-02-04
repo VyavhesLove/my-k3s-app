fbadc3ff1e6afdf402cbc2c60120ea2311ab810a
Your Name <your@email.com>
2026-02-04 14:52:03 +0500

    add ORM and swagger

--- backend/inventory/settings.py
+++ backend/inventory/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework',
     'drf_spectacular',
     'items',
 ]
--- backend/items/serializers.py
+++ backend/items/serializers.py
@@ -1,8 +1,24 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Item, Location
+
 
 class ItemSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели ТМЦ"""
     class Meta:
         model = Item
-        fields = ['id', 'name', 'serial', 'brand', 'status', 'responsible', 'location', 'qty']
+        fields = '__all__'
+
+
+class LocationSerializer(serializers.ModelSerializer):
+    """Сериализатор для локаций"""
+    class Meta:
+        model = Location
+        fields = ['id', 'name']
+
+
+class StatusCounterSerializer(serializers.Serializer):
+    """Схема для счетчиков уведомлений"""
+    to_receive = serializers.IntegerField()
+    to_repair = serializers.IntegerField()
+    issued = serializers.IntegerField()
 
